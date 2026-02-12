6e4843a5f0f6ae05bc73b2b087b5f6f90016714f
Santiago Molina <santi020k@gmail.com>
2026-02-12 11:16:17 -0500

    feat: Refine NestJS ESLint configuration to target TypeScript files and establish a NestJS playground with Swagger and improved ESLint/TypeScript setup

--- packages/playground/nest/src/app.controller.ts
+++ packages/playground/nest/src/app.controller.ts
@@ -1,8 +1,11 @@
 import { Controller, Get } from '@nestjs/common'
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger'
 
+@ApiTags('app')
 @Controller()
 export class AppController {
   @Get()
+  @ApiOkResponse({ description: 'Hello world' })
   getHello(): string {
     return 'Hello World!'
   }
